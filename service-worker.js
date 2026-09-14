16a312e7cf87fc0f05e8af4786c5a59bed01bcd1
erikanter <erikanter2020@gmail.com>
2026-09-14 16:34:40 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='aqua-cube-v0.1.1';
+const CACHE='aqua-cube-v0.2-history';
 const CORE=['./','./index.html','./manifest.webmanifest','./service-worker.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
