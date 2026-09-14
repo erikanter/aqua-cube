9c5f202b3d3ab97a4587af8725fdbdd98a4f513c
erikanter <erikanter2020@gmail.com>
2026-09-14 18:37:16 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,2 @@
-const CACHE='aqua-cube-v0.6.3-no-folder';
-const CORE=['./','./index.html','./manifest.webmanifest','./service-worker.js','./aqua-cube-icons.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();const clients=await self.clients.matchAll({includeUncontrolled:true});clients.forEach(c=>c.navigate(c.url));})()));
