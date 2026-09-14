ae2a0add069680c386bb34c588b58b5054f049dc
erikanter <erikanter2020@gmail.com>
2026-09-14 20:04:03 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="aqua-cube-v9-2";
+const VERSION="aqua-cube-v10-2";
 const STATIC=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./apple-touch-icon-v2.png","./cube-shell.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(VERSION).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)));await self.clients.claim()})()));
