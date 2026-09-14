7ecdc8b8f577d18efb1c709352b9232ac42d25fc
erikanter <erikanter2020@gmail.com>
2026-09-14 17:37:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='aqua-cube-v0.5-final-icon';
-const CORE=['./','./index.html','./manifest.webmanifest','./service-worker.js'];
+const CACHE='aqua-cube-v0.6-approved-icons';
+const CORE=['./','./index.html','./manifest.webmanifest','./service-worker.js','./aqua-cube-icons.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
